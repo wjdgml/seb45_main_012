e59b746930c2f68404c75ced6d65190cc3c031dd
matchaing <alswn202020@gmail.com>
2023-08-30 09:27:11 +0900

    주석 추가

--- client/src/store/menuSlice.js
+++ client/src/store/menuSlice.js
@@ -5,9 +5,11 @@ const initialState = {
 //   activeMenu: '전체글보기',
 };
 
+//슬라이스 생성
 const menuSlice = createSlice({
   name: 'menu',
   initialState,
+  //리듀서 함수 정의
   reducers: {
     setActiveMenu: (state, action) => {
       state.activeMenu = action.payload;
@@ -15,8 +17,9 @@ const menuSlice = createSlice({
   },
 });
 
+// 액션 생성자를 내보냅니다
 export const { setActiveMenu } = menuSlice.actions;
-
+// 선택자 함수를 내보냅니다 (현재 활성 메뉴를 선택하는 역할)
 export const selectActiveMenu = (state) => state.menu.activeMenu;
-
+// 리듀서 함수를 내보냅니다 (상태관리 수행하는 역할)
 export default menuSlice.reducer;
