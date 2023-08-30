7c202ee90383d17fddabf4a83175452b4bb438b6
matchaing <alswn202020@gmail.com>
2023-08-30 16:43:37 +0900

    feat : Modal store 추가

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,18 @@
+import '../styles/Button.css';
+
+const Modal = () => {
+
+    return (
+    <>
+        <div className='modal_container'>
+            
+            <div>
+                <button class="custom_button confirm_button"></button>
+                <button class="custom_button cancel_button"></button>
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default Modal;
