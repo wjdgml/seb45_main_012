0b9fccdf8b9a411f8fbe04605c4f52a78755d9d1
matchaing <alswn202020@gmail.com>
2023-09-01 11:01:24 +0900

    feat : 버튼 만들기

---
+++ client/src/pages/FreeBoardPage.jsx
@@ -0,0 +1,21 @@
+import '../styles/Button.css';
+
+const FreeBoardPage = () => {
+
+    return (
+    <>
+        <div className='page_container'>
+            
+            <div>
+                <button class="custom_board_button confirm_button">자유 게시판</button>
+                {/* <button class="custom_board_button cancel_button">자유 게시판</button> */}
+            </div>
+            <div className=''>
+                
+            </div>
+        </div>
+    </>
+    )
+}
+
+export default FreeBoardPage;
