13ccb98fb0a64c65f24956061e89a1c74513c240
jumpm9239 <jumpm9239@gmail.com>
2023-09-06 17:08:31 +0900

    chore: refresh 함수 분리

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -4,31 +4,29 @@ import Cookie from 'js-cookie';
 axios.defaults.withCredentials = true;
 
 const Instance = axios.create({
-    baseURL: '효성님이 전달해주셨어야 하는...',
+    baseURL: '배포 URL',
 });
 
-Instance.interceptors.request.use(
+export const refresh =   async (config) => {
 
-    async (config) => {
+    const refreshToken = Cookie.get('refreshToken');
+    const expiresAt = localStorage.getItem('expiresAt');
+    const currentTime = Math.floor(Date.now() / 1000);
 
-        const refreshToken = Cookie.get('refreshToken');
-        const expiresAt = localStorage.getItem('expiresAt');
-        const currentTime = Math.floor(Date.now() / 1000);
+    if (expiresAt < currentTime) {
+        console.log("토큰 만료, 재발급 요청");
+        const res = await Instance.post("/refresh", {
+            headers: { Refresh: refreshToken, }
+        });
+        const { newAccessToken } = res.data;
+        console.log("accessToken 재발급 성공: ", newAccessToken) // 확인하고 지우기
+        localStorage.setItem('accessToken: ', newAccessToken);
+        localStorage.setItem() // expiresAt 받아오기
+        config.headers["Authorization"] = `Bearer ${newAccessToken}`; // 토큰 교체
+    }
+    return config;
+}
 
-        if (expiresAt < currentTime) {
-            console.log("토큰 만료, 재발급 요청");
-            const res = await Instance.post("/refresh", {
-                headers: { Refresh: refreshToken, }
-            });
-            const { newAccessToken } = res.data;
-            console.log("accessToken 재발급 성공: ", newAccessToken) // 확인하고 지우기
-            localStorage.setItem('accessToken: ', newAccessToken);
-            localStorage.setItem() // expiresAt 받아오기
-            config.headers["Authorization"] = `Bearer ${newAccessToken}`; // 토큰 교체
-        }
-        return config;     
-    }   
-
-)
+Instance.interceptors.request.use(refresh)
 
 export default Instance;
