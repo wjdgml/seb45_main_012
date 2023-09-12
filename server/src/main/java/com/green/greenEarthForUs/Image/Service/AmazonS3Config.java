3e1e9cf132d4a7da3ea29c9fda07326d8e4bbf34
wjdgml <wjdgml2848@gmail.com>
2023-09-12 13:38:07 +0900

    fix : user, post 이미지 첨부 코드 수정

--- server/src/main/java/com/green/greenEarthForUs/Image/Service/AmazonS3Config.java
+++ server/src/main/java/com/green/greenEarthForUs/Image/Service/AmazonS3Config.java
@@ -2,16 +2,10 @@ package com.green.greenEarthForUs.Image.Service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.green.greenEarthForUs.Exception.ImageUploadException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.UUID;
+
 
 @Configuration
 public class AmazonS3Config {
