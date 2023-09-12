4f4459c7118520e5467820aa7e8564efe16fd4cd
windeer9 <young7362@gmail.com>
2023-09-12 15:36:47 +0900

    feat: spring multipart setting

--- server/src/main/java/com/green/greenEarthForUs/user/Entity/User.java
+++ server/src/main/java/com/green/greenEarthForUs/user/Entity/User.java
@@ -19,7 +19,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
