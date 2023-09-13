f715a14c2611dd663218df6961b1fe9aa53e7113
yuumni <minyunj77@gmail.com>
2023-09-13 20:11:10 +0900

    feat : 자유게시판만 API 통신 성공

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -9,13 +9,25 @@ export const getPosts = (page) => {
   return instance.get(`/post/free?page=${page}`);
 };
 
-export const postPosts = (type, title, body, file, open, user_id) => {
-  return instance.post(`/post/${user_id}`, {
+export const postPosts = (type, title, body, open, img = null) => {
+  const formData = new FormData();
+
+  const jsonData = {
     type: type,
     title: title,
     body: body,
-    file: file,
     open: open
+  };
+
+  formData.append('json', new Blob([JSON.stringify(jsonData)], { type: 'application/json' }));
+  
+  formData.append('image', img)
+  formData.append('image', img)
+
+  return instance.post(`/post/10`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    }
   });
 }
 
--- client/src/components/PostEditer.jsx
+++ client/src/components/PostEditer.jsx
@@ -3,15 +3,13 @@ import '../styles/PostEditer.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { postPosts } from "api/api.js";
-import axios from "axios";
 
 function PostEditer() {
   const [formData, setFormData] = useState({
     type: 'free',
     title: '',
     body: '',
-    file: null,
-    open: 'open'
+    open: 'true'
   });
 
   const handleInputChange = (e) => {
@@ -20,9 +18,7 @@ function PostEditer() {
   };
 
   const handleCreatePost = () => {
-
-
-    postPosts(formData.type, formData.title, formData.body, formData.file, formData.open, )
+    postPosts(formData.type, formData.title, formData.body, formData.open)
     .then((resp) => {
       console.log('성공~!!', resp.data);
     })
@@ -43,20 +39,65 @@ function PostEditer() {
 }
 
 function PostEditerWithImage() {
+  const [formData, setFormData] = useState({
+    type: 'image',
+    title: '',
+    body: '',
+    open: 'true'
+  });
+  const [previewImage, setPreviewImage] = useState(null);
 
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0]; // 선택한 파일의 첫 번째 파일을 가져옴
+    setFormData({ ...formData, file });
+
+      // 이미지 파일을 읽기
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        // 읽은 이미지 데이터를 사용하여 미리보기 업데이트
+        setPreviewImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleCreatePost = () => {
+    postPosts(formData.type, formData.title, formData.body, formData.open, previewImage)
+    .then((resp) => {
+      console.log('성공~!!!!!', resp.data);
+    })
+    .catch((err) => {
+      console.error('실패ㅠㅠ', err);
+    });
+  }
   
 
   return (
     <div className="post_editer_with_image">
 
       <div className="image_upload_form">
-        <FontAwesomeIcon className='plus_icon' icon={faPlus}/>이미지
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleFileInputChange}
+        />
+        {previewImage && <img src={previewImage} alt="미리보기"/>}
+        {/* {console.log(previewImage)} */}
+        <div className={`plus_image_icon ${previewImage ? 'clear' : ''}`} >
+          <FontAwesomeIcon className='plus_icon' icon={faPlus}/>이미지
+        </div>
       </div>
 
       <div className="editer_wrapper">
-        <button className="post_complete_btn">작성</button>
-        <input type="text" className="post_title_input" placeholder="제목을 입력하세요"/>
-        <textarea className='post_text_area' name="" id="" cols="30" rows="25" placeholder="내용을 입력하세요."></textarea>
+      <button onClick={handleCreatePost} className="post_complete_btn wide">작성</button>
+        <input onChange={handleInputChange} name='title' value={formData.title} type="text" className="post_title_input" placeholder="제목을 입력하세요"/>
+        <textarea onChange={handleInputChange} name='body' value={formData.body} className='post_text_area' placeholder="내용을 입력하세요."></textarea>
       </div>
     
     </div>
