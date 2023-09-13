16c6bd0f47672b8d1dfbf1497013ff74d743d551
windeer9 <young7362@gmail.com>
2023-09-13 18:26:24 +0900

    refact: post delete refact

--- server/src/main/java/com/green/greenEarthForUs/post/Service/PostService.java
+++ server/src/main/java/com/green/greenEarthForUs/post/Service/PostService.java
@@ -168,7 +168,7 @@ public class PostService {
 
         // 작성자 요청자 일치하는지
         Long id = existingPost.getUser().getUserId();
-        if (id.equals(userId)) {
+        if (id != userId) {
             throw new UnauthorizedException("You are not authorized to delete this post");
         }
 
