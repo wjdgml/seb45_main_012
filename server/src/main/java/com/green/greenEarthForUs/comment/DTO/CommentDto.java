0c27fd9ddf5a65ea9671b51b016b1f473ce72f79
wjdgml <wjdgml2848@gmail.com>
2023-09-14 11:32:51 +0900

    fix : commnet 코드 수정중

--- server/src/main/java/com/green/greenEarthForUs/comment/DTO/CommentDto.java
+++ server/src/main/java/com/green/greenEarthForUs/comment/DTO/CommentDto.java
@@ -1,22 +1,29 @@
 package com.green.greenEarthForUs.comment.DTO;
 
+import com.green.greenEarthForUs.post.Entity.Post;
+import com.green.greenEarthForUs.user.Entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentDto {
 
     private Long id;
 
-    private String body;
-
     private Long postId;
 
     private Long userId;
+
+    private String body;
 
     private LocalDateTime createdAt;
 }
--- server/src/main/java/com/green/greenEarthForUs/post/Repository/PostRepository.java
+++ server/src/main/java/com/green/greenEarthForUs/post/Repository/PostRepository.java
@@ -9,13 +9,10 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    // 공개 게시물
-    //List<Post> findByOpenTrue();
-
-    // 비공개 게시물
-    //List<Post> findByOpenFalse();
 
     List<Post> findByType(String typeName);
 
     List<Post> findByUser(User user);
+
+
 }
