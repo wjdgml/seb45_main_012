2ed5e5344bd0d47b2dcfe0e04bf97c3668022ff6
wjdgml <wjdgml2848@gmail.com>
2023-09-14 13:56:10 +0900

    fix : commnet 코드 수정중

--- server/src/main/java/com/green/greenEarthForUs/comment/DTO/CommentResponseDto.java
+++ server/src/main/java/com/green/greenEarthForUs/comment/DTO/CommentResponseDto.java
@@ -1,21 +1,16 @@
 package com.green.greenEarthForUs.comment.DTO;
 
 
-import com.green.greenEarthForUs.post.Entity.Post;
-import com.green.greenEarthForUs.user.Entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.*;
 import java.time.LocalDateTime;
 @Getter
 @Setter
 @AllArgsConstructor
 public class CommentResponseDto {
 
-    private Long id;
+    private Long commentId;
 
     private Long userId;
 
