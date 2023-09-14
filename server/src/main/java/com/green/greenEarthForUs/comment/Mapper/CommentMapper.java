e91c9c1ae5da35251014917e21dfdb17ce87e98d
windeer9 <young7362@gmail.com>
2023-09-14 14:58:40 +0900

    refact: comment mapping refact

--- server/src/main/java/com/green/greenEarthForUs/comment/Mapper/CommentMapper.java
+++ server/src/main/java/com/green/greenEarthForUs/comment/Mapper/CommentMapper.java
@@ -6,17 +6,20 @@ import com.green.greenEarthForUs.comment.DTO.CommentDto;
 import com.green.greenEarthForUs.comment.DTO.CommentResponseDto;
 import com.green.greenEarthForUs.comment.Entity.Comment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
-
+    @Mapping(source = "user.userId", target = "userId")
+    @Mapping(source = "post.postId", target = "postId")
     CommentResponseDto commentToResponseDto(Comment comment);
 
+    @Mapping(source = "user.userId", target = "userId")
+    @Mapping(source = "post.postId", target = "postId")
     List<CommentResponseDto> commentListToResponseDtoList(List<Comment> comments);
 
-    CommentDto commentToCommentDto(Comment comment);
 
 }
